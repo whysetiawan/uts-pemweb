b13b2d42013ab48d8b77d74dcaf1daaff7d00486
Hibar Taufikurachman <hibartaufikurachman@gmail.com>
2021-03-20 15:04:29 +0700

    Hibar create validate.js

---
+++ validate.js
@@ -0,0 +1,23 @@
+function validate(formArray) {
+    let error = []
+    formArray.forEach(element => {
+      let form = document.getElementById(element);
+      if (!form.value) {
+        error.push(element);
+        document.getElementById(`${element}-error`).innerHTML = `${element} harus di isi`
+      }
+      else {
+        document.getElementById(`${element}-error`).innerHTML = ``
+      }
+    });
+    if (error.length < 1) {
+      var username = document.getElementById("Username").value;
+      var password = document.getElementById("Password").value;
+      if (username == "admin" && password == "admin123") {
+        window.location.href = 'home.html'
+      }
+      else {
+        alert("Password/Username anda salah!");
+      }
+    }
+  }
