c38a468fcd0917a2767c22cade24b3feaaa0a783
Wahyu Setiawan <whysetiawan27@gmail.com>
2021-03-23 16:16:06 +0700

    Push

---
+++ script/products.js
@@ -0,0 +1,24 @@
+const productArray = [
+  {
+    nama: "Coffee",
+    jenis: "Drinks",
+    harga_produksi: "Rp 15.000",
+    harga_jual: "Rp 20.000",
+  },
+  {
+    nama: "Seblak",
+    jenis: "Foods",
+    harga_produksi: "Rp 15.000",
+    harga_jual: "Rp 20.000",
+  },
+  {
+    nama: "Coffee",
+    jenis: "Drinks",
+    harga_produksi: "Rp 15.000",
+    harga_jual: "Rp 20.000",
+  }
+];
+
+function pushProducts(product) {
+  productArray.push(product)
+}
